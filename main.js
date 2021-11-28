8a446f127e5ab3ce76683ae113b85de66ff40159
aram20134 <dasigty@gmail.com>
2021-11-28 17:44:27 +0300

    mastre

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.side-nav li').click(function() {
+        if(!$(this).hasClass("is-active")) {
+            if ($('.side-nav li').hasClass('is-active')) {
+                $('.side-nav li').removeClass('is-active');
+                $('.side-nav li').addClass('none-active');
+            }
+            $(this).removeClass('none-active');
+            $(this).toggleClass('is-active');
+        }
+    });    
+});
+
